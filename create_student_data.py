cdd75f993ad4a91708a1534fd396f18edefd02e0
wasimshaikh89400 <72592698+wasimshaikh89400@users.noreply.github.com>
2021-05-15 15:58:58 +0530

    Update create_student_data.py

--- create_student_data.py
+++ create_student_data.py
@@ -8,10 +8,9 @@ myTable = myDB["studentData"] # Create collection For student
 
 dictionary = [{ "firstName" : "wasim" ,  "lastname" : "shaikh" , "DOB" : "26 November 1998" , "class" : "MCA II" , "Roll_no" : "51"} ,
               { "firstName" : "vishal" ,  "lastname" : "nangare" , "DOB" : "13 June 1998" , "class" : "MCA III" , "Roll_no" : "21"},
-              { "firstName" : "krishna" ,  "lastname" : "attal" , "DOB" : "26 March 1997" , "class" : "MCA I" , "Roll_no" : "36"},
+              { "firstName" : "krishna" ,  "lastname" : "attal" , "DOB" : "26 March 1997" , "class" : "MCA I" , "Roll_no" : "4"},
               { "firstName" : "swapnil" ,  "lastname" : "jadhav" , "DOB" : "21 May 1999" , "class" : "MCA II" , "Roll_no" : "11"},
-              {"firstName" : "ajay" ,  "lastname" : "kanade" , "DOB" : "26 may 1998" , "class" : "MCA II" , "Roll_no" : "3"},
-              {"firstName" : "ajay" ,  "lastname" : "kanade" , "DOB" : "26 may 1998" , "class" : "MCA II" , "Roll_no" : "4"}]
+              {"firstName" : "ajay" ,  "lastname" : "kanade" , "DOB" : "26 may 1998" , "class" : "MCA II" , "Roll_no" : "3"}]
 
 x = myTable.insert_many(dictionary)  # add data into collection
 
