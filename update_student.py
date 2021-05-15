dfe35ebc9cb2e7427fa4413311d69be6868a67fe
wasimshaikh89400 <72592698+wasimshaikh89400@users.noreply.github.com>
2021-05-15 16:16:55 +0530

    Update update_student.py

--- update_student.py
+++ update_student.py
@@ -1,5 +1,3 @@
-import  pymongo
-
 myConnection = pymongo.MongoClient("mongodb://localhost:27017/") #Create connection to mongoDB
 
 myDB = myConnection["StudentDatabase"] #create database for student data
@@ -7,11 +5,19 @@ myDB = myConnection["StudentDatabase"] #create database for student data
 myTable = myDB["studentData"] # Create collection For student
 
 query = {"Roll_no" : "4"}
+
+
+x = myTable.find(query)
+
+for y in x:
+  print("Before update " + str(y))
+
+
 updateQuery = {"$set" : {"DOB" : "20 December 1996"}}
 
 x = myTable.update_one( query , updateQuery)
 
-x = myTable.find({} , {"DOB":"20 December 1996" , "Roll_no" :1})
+x = myTable.find(query)
 
 for y in x:
-    print(y)
+  print("After update " + str(y))
